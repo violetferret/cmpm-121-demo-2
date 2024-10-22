f2dfe14376cc66c4f964294846519f5f88227e48
violetferret <kahlstro@ucsc.edu>
2024-10-21 23:52:09 -0700

    step 1 complete: created non interactive canvas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
 
-const APP_NAME = "Wassup";
+const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// app.innerHTML = APP_NAME;
+
+// add game title
+const gameName = "Sticker Sketchpad";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+// add canvas
+// from 
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+app.append(canvas);
+
+
