adc33e8a7b8966fc3fd8e452456d5cdab59839c4
violetferret <kahlstro@ucsc.edu>
2024-11-02 17:33:26 -0700

    step 5 complete: implemented draw commands as interface/class

--- src/main.ts
+++ src/main.ts
@@ -26,37 +26,58 @@ app.append(canvas);
 let isDrawing = false;
 let x = 0;
 let y = 0;
-let index = 0;
 
 interface Point {
   x: number;
   y: number;
 }
 type Line = Point[];
-let lines: Line[] = [];
 
 let currentPoint: Point;
 let currentLine: Line = [];
+let currentCommand: drawCommand;
+
+let commandStack: drawCommand[] = [];
+const redoStack: drawCommand[] = [];
 
 const drawingChanged = new Event("drawing-changed");
 
-canvas.addEventListener("mousedown", (e) => {
-  if (isDrawing == false) {
-    currentLine = [];
-    isDrawing = true;
-    x = e.offsetX;
-    y = e.offsetY;
-    currentPoint = { x, y };
-    currentLine.push(currentPoint);
-    lines.push(currentLine);
-    console.log(lines.length);
-    return;
+interface drawCommand {
+  execute(): void;
+}
+
+class LineDrawCommand implements drawCommand {
+  ctx: CanvasRenderingContext2D;
+  line: Point[];
+
+  constructor(ctx: CanvasRenderingContext2D, line: Point[]) {
+    this.ctx = ctx;
+    this.line = line;
+    this.ctx.strokeStyle = "black";
+    this.ctx.lineWidth = 1;
   }
+
+  execute(): void {
+    for (let i = 0; i < this.line.length - 1; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.line[i].x, this.line[i].y);
+      this.ctx.lineTo(this.line[i + 1].x, this.line[i + 1].y);
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+  }
+}
+
+canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
+  currentLine = [];
   x = e.offsetX;
   y = e.offsetY;
   currentPoint = { x, y };
-  lines[index].push(currentPoint);
+  currentLine.push(currentPoint);
+  currentCommand = new LineDrawCommand(canvasContext, currentLine);
+  commandStack.push(currentCommand);
+  canvas.dispatchEvent(drawingChanged);
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -64,49 +85,24 @@ canvas.addEventListener("mousemove", (e) => {
     x = e.offsetX;
     y = e.offsetY;
     currentPoint = { x, y };
-    lines[index].push(currentPoint);
-
+    currentLine.push(currentPoint);
     canvas.dispatchEvent(drawingChanged);
   }
 });
 
-canvas.addEventListener("mouseup", (e) => {
+canvas.addEventListener("mouseup", function() {
   isDrawing = false;
-  index++;
-});
+  // commandStack.push(new LineDrawCommand(canvasContext, currentLine));
+  canvas.dispatchEvent(drawingChanged);
+})
 
-canvas.addEventListener("drawing-changed", (e) => {
+canvas.addEventListener("drawing-changed", function() {
   canvasContext.clearRect(0, 0, 256, 256);
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines[i].length - 1; j++) {
-      drawLine(
-        canvasContext,
-        lines[i][j].x,
-        lines[i][j].y,
-        lines[i][j + 1].x,
-        lines[i][j + 1].y
-      );
-    }
-  }
-});
+  commandStack.forEach((command) => {
+    command.execute();
+  })
+})
 
-function drawLine(
-  canvasContext: CanvasRenderingContext2D,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number
-) {
-  canvasContext.beginPath();
-  canvasContext.strokeStyle = "black";
-  canvasContext.lineWidth = 1;
-  canvasContext.moveTo(x1, y1);
-  canvasContext.lineTo(x2, y2);
-  canvasContext.stroke();
-  canvasContext.closePath();
-}
-
-// add a break between
 const br = document.createElement("br");
 app.append(br);
 
@@ -116,43 +112,32 @@ const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear canvas";
 app.append(clearButton);
 
-clearButton.addEventListener("click", function () {
+clearButton.addEventListener("click", function() {
   canvasContext.clearRect(0, 0, 256, 256);
+  commandStack = [];
   currentLine = [];
-  lines = [];
-  index = 0;
-  undoLines = [];
-});
+})
 
 // undo button
 const undoButton = document.createElement("button");
 undoButton.innerHTML = "Undo";
 app.append(undoButton);
 
-let undoLines: Line[] = [];
-let undoLine: Line;
-
 undoButton.addEventListener("click", function () {
-  if (lines.length > 0) {
-    undoLine = lines.pop() as Line;
-    index--;
-    undoLines.push(undoLine);
-    canvas.dispatchEvent(drawingChanged);
-  }
-});
+    if (commandStack.length > 0) {
+      redoStack.push(commandStack.pop() as drawCommand);
+      canvas.dispatchEvent(drawingChanged);
+    }
+  });
 
-// redo button
+  // redo button
 const redoButton = document.createElement("button");
 redoButton.innerHTML = "Redo";
 app.append(redoButton);
 
-let redoLine: Line;
-
 redoButton.addEventListener("click", function () {
-  if (undoLines.length > 0) {
-    redoLine = undoLines.pop() as Line;
-    lines.push(redoLine);
-    index++;
-    canvas.dispatchEvent(drawingChanged);
-  }
-});
+    if (redoStack.length > 0) {
+      commandStack.push(redoStack.pop() as drawCommand);
+      canvas.dispatchEvent(drawingChanged);
+    }
+  });
